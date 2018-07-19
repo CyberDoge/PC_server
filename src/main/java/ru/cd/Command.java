395913069d43ef8ccde15bd553bb051c7604baf7
CyberDoge <tagirman998@gmail.com>
2018-07-19 23:36:35 +0400

    req, resp

--- src/main/java/ru/cd/Command.java
+++ src/main/java/ru/cd/Command.java
@@ -1,47 +1,56 @@
 package ru.cd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.Closeable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
 
-public class Command implements Runnable {
+public class Command implements Runnable, Closeable {
 
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    private static final Runtime RUNTIME = Runtime.getRuntime();
+    private static int lastId = 0;
 
-    private final static Runtime RUNTIME = Runtime.getRuntime();
     private Connection connection;
+
     private String command;
     private long startingDate;
     private Process process;
-    private InputStream reader;
-    private OutputStream writer;
+    private InputStream processReader;
+    private OutputStream processWriter;
+    private int id;
 
     public Command(Connection connection, String command) {
         this.connection = connection;
         this.command = command;
         startingDate = new Date().getTime();
+        id = lastId++;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public boolean isAlive() {
+        return process.isAlive();
+    }
 
     @Override
     public void run() {
         try {
             System.out.println(ANSI_BLUE + command + ANSI_RESET);
             process = RUNTIME.exec(command);
-            reader = process.getInputStream();
-            writer = process.getOutputStream();
-            var result = new String(reader.readAllBytes());
-            System.out.println(ANSI_GREEN + result + ANSI_RESET);
-            connection.writeResult(result);
+            processReader = process.getInputStream();
+            processWriter = process.getOutputStream();
+            var output = new String(processReader.readAllBytes());
+            System.out.println(ANSI_GREEN + output + ANSI_RESET);
+            lifeCycle();
         } catch (IOException e) {
             try {
                 connection.writeResult(command + " exec error:\n" + e.getLocalizedMessage());
@@ -52,7 +61,48 @@ public class Command implements Runnable {
         }
     }
 
-    public void sendInfo() {
-        StringBuilder builder = new StringBuilder();
+    private void sendResponse(int id, long pid, long startingDate, String output) throws IOException {
+        var wrapper = new CommandJsonWrapper(id, pid, startingDate, output);
+        var objMapper = new ObjectMapper();
+        var res = objMapper.writeValueAsString(wrapper);
+        connection.writeResult(res);
+    }
+
+    private void lifeCycle() throws IOException {
+        while (process.isAlive()) {
+            var processOutput = new String(processReader.readAllBytes());
+            sendResponse(id, process.pid(), startingDate, processOutput);
+        }
+    }
+
+
+    public void getRequest(String input) throws IOException {
+        if (input.equals("kill")) {
+            close();
+            return;
+        }
+        processWriter.write(input.getBytes());
+
+    }
+
+    @Override
+    public void close() throws IOException {
+        processReader.close();
+        processWriter.close();
+        process.destroy();
+    }
+
+    private static final class CommandJsonWrapper {
+        String output;
+        private int id;
+        private long pid;
+        private long startingDate;
+
+        public CommandJsonWrapper(int id, long pid, long startingDate, String output) {
+            this.id = id;
+            this.pid = pid;
+            this.startingDate = startingDate;
+            this.output = output;
+        }
     }
 }
--- src/main/java/ru/cd/Main.java
+++ src/main/java/ru/cd/Main.java
@@ -2,14 +2,27 @@ package ru.cd;
 
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
+    private static final Map<Integer, Command> COMMANDS = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         Connection connection = new Connection();
-       while (true){
-           String command = connection.readCommand();
-           Command cmd = new Command(connection, command);
-           new Thread(cmd).start();
-       }
+
+        while (true) {
+            String input = connection.readCommand();
+            if (input.startsWith("id:")) {
+                var indexOfStart = input.indexOf(',');
+                var id = Integer.parseInt(input.substring(3, indexOfStart));
+                var command = COMMANDS.get(id);
+                var request = input.substring(indexOfStart+1);
+                command.getRequest(request);
+            }
+            Command cmd = new Command(connection, input);
+            COMMANDS.put(cmd.getId(), cmd);
+            new Thread(cmd).start();
+        }
     }
 }
